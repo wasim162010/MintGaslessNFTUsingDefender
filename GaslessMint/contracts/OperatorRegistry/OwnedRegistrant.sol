403d5f87f1a262a6430d1ce27644a89175f3fd41
wasim162010 <47940538+wasim162010@users.noreply.github.com>
2023-08-08 03:09:30 -0700

    Add files via upload

---
+++ GaslessMint/contracts/OperatorRegistry/OwnedRegistrant.sol
@@ -0,0 +1,64 @@
+
+pragma solidity ^0.8.13;
+
+import {IOperatorFilterRegistry} from "./IOperatorFilterRegistry.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+// import {Ownable2Step} from "./Ownable2Step.sol";
+
+/**
+ * @title  OwnedRegistrant
+ * @notice Ownable contract that registers itself with the OperatorFilterRegistry and administers its own entries,
+ *         to facilitate a subscription whose ownership can be transferred.
+ */
+
+
+ abstract contract Ownable2Step is Ownable {
+    address private _pendingOwner;
+
+    event OwnershipTransferStarted(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Returns the address of the pending owner.
+     */
+    function pendingOwner() public view virtual returns (address) {
+        return _pendingOwner;
+    }
+
+    /**
+     * @dev Starts the ownership transfer of the contract to a new account. Replaces the pending transfer if there is one.
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual override onlyOwner {
+        _pendingOwner = newOwner;
+        emit OwnershipTransferStarted(owner(), newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`) and deletes any pending owner.
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual override {
+        delete _pendingOwner;
+        super._transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev The new owner accepts the ownership transfer.
+     */
+    function acceptOwnership() external {
+        address sender = _msgSender();
+        require(pendingOwner() == sender, "Ownable2Step: caller is not the new owner");
+        _transferOwnership(sender);
+    }
+}
+
+
+contract OwnedRegistrant is Ownable2Step {
+    address constant registry = 0x000000000000AAeB6D7670E522A718067333cd4E;
+
+    constructor(address _owner) {
+        IOperatorFilterRegistry(registry).register(address(this));
+        transferOwnership(_owner);
+    }
+}
