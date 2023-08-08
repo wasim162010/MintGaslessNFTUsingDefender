524f0eec6a763b78eb02d730d178e08eb06c0443
wasim162010 <47940538+wasim162010@users.noreply.github.com>
2023-08-08 03:02:08 -0700

    Add files via upload

---
+++ OperatorRegistry/OperatorFilterer.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IOperatorFilterRegistry} from "./IOperatorFilterRegistry.sol";
+
+contract OperatorFilterer {
+    error OperatorNotAllowed(address operator);
+
+    IOperatorFilterRegistry constant OPERATOR_FILTER_REGISTRY =
+        IOperatorFilterRegistry(0x000000000000AAeB6D7670E522A718067333cd4E);
+
+    constructor(address subscriptionOrRegistrantToCopy, bool subscribe) {
+        // If an inheriting token contract is deployed to a network without the registry deployed, the modifier
+        // will not revert, but the contract will need to be registered with the registry once it is deployed in
+        // order for the modifier to filter addresses.
+        if (address(OPERATOR_FILTER_REGISTRY).code.length > 0) {
+            if (subscribe) {
+                OPERATOR_FILTER_REGISTRY.registerAndSubscribe(address(this), subscriptionOrRegistrantToCopy);
+            } else {
+                if (subscriptionOrRegistrantToCopy != address(0)) {
+                    OPERATOR_FILTER_REGISTRY.registerAndCopyEntries(address(this), subscriptionOrRegistrantToCopy);
+                } else {
+                    OPERATOR_FILTER_REGISTRY.register(address(this));
+                }
+            }
+        }
+    }
+
+    modifier onlyAllowedOperator() virtual {
+        // Check registry code length to facilitate testing in environments without a deployed registry.
+        if (address(OPERATOR_FILTER_REGISTRY).code.length > 0) {
+            if (!OPERATOR_FILTER_REGISTRY.isOperatorAllowed(address(this), msg.sender)) {
+                revert OperatorNotAllowed(msg.sender);
+            }
+        }
+        _;
+    }
+}
